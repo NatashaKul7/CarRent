e29128c1d139c13eeb29e85fb8ce89cefe6e93d5
NatashaKul7 <125500989+NatashaKul7@users.noreply.github.com>
2024-02-15 10:47:01 +0000

    Deploying to gh-pages from @ NatashaKul7/CarRent@e04b869d744dbe36987ed477f182494682637caa 🚀

---
+++ assets/FavoritesPage-BhsQ7t4f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cxb13eOE.js";const t=()=>e.jsx("div",{children:"FavoritesPage"});export{t as default};
---
+++ assets/HomePage-CBNUV8Q0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cxb13eOE.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
